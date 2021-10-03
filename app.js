f0fbc0dc689a5b08ca53bdeb0835bf59a02da7c0
Rianto <rianto.rnt@gmail.com>
2021-10-03 11:27:00 +0800

    Routing request

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  //process.exit();
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title> My Firts Page </title></head>');
-  res.write('<body></h1>Hello from node.js Server 2021</h1></body>');
-  res.write('</html>');
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title> Enter Message </title></head>");
+    res.write('<body><form action="/message" method="POST"><input type="text"><button type="text">Send</form></body>');
+    res.write('</html>');
+    res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title> My Firts Page </title></head>");
+  res.write("<body></h1>Hello from node.js Server 2021</h1></body>");
+  res.write("</html>");
   res.end();
 });
 
